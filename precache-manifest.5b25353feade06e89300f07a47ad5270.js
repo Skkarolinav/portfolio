2aaa66af16d6cac372f755679bc8311a0b70c8cb
SKarolinav <Skkarolinav@gmail.com>
2019-06-10 05:37:54 +0200

    Updates

---
+++ precache-manifest.5b25353feade06e89300f07a47ad5270.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342e8267855cdd74ecf24c9363a8424b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "15a16a6887255a2dba20",
+    "url": "/portfolio/static/css/main.0e5ffc70.chunk.css"
+  },
+  {
+    "revision": "adbe3fa7d54125cdb4cc",
+    "url": "/portfolio/static/js/2.313606f0.chunk.js"
+  },
+  {
+    "revision": "15a16a6887255a2dba20",
+    "url": "/portfolio/static/js/main.234c6056.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "068a35e39dd75f8aae075276da335de9",
+    "url": "/portfolio/static/media/Background_Contact.068a35e3.jpg"
+  },
+  {
+    "revision": "f59521982296c9471ce0f0abdc268ed5",
+    "url": "/portfolio/static/media/Background_Home.f5952198.jpg"
+  },
+  {
+    "revision": "5f5127a0a7b8737b51fb13e8f7a61aa0",
+    "url": "/portfolio/static/media/Kit_2D_Histogram_Application.5f5127a0.png"
+  },
+  {
+    "revision": "bbde4128826497159cc82a50bccfc712",
+    "url": "/portfolio/static/media/Kit_Dentistry_Website.bbde4128.png"
+  },
+  {
+    "revision": "54e92114ace70a668c9dc711713ecc30",
+    "url": "/portfolio/static/media/Kit_Exchange_Currency_Application.54e92114.png"
+  },
+  {
+    "revision": "74b99f8a79680d6c9dd6152e6c1f6b46",
+    "url": "/portfolio/static/media/Kit_Interior_Finishing_Website.74b99f8a.png"
+  },
+  {
+    "revision": "1dedff46a14509a028fc1a7fcf8c96be",
+    "url": "/portfolio/static/media/Kit_Photography_Website.1dedff46.png"
+  },
+  {
+    "revision": "d92fb158558293376b3f9fe75575cb89",
+    "url": "/portfolio/static/media/Kit_Weather_Application.d92fb158.png"
+  },
+  {
+    "revision": "172980a3d4000610e439f7c000e180f2",
+    "url": "/portfolio/static/media/My_CV.172980a3.pdf"
+  },
+  {
+    "revision": "d8545a6b3b54aee1f629ca1e323fb342",
+    "url": "/portfolio/static/media/Photo_of_Me.d8545a6b.jpg"
+  },
+  {
+    "revision": "831449013962986c8f3f1fe2df3c8de0",
+    "url": "/portfolio/static/media/SVG_Bootstrap.83144901.svg"
+  },
+  {
+    "revision": "3f8cea072f81577e77ef588e866a2111",
+    "url": "/portfolio/static/media/SVG_CSS.3f8cea07.svg"
+  },
+  {
+    "revision": "fae70cd6361a3cbc4cacc6cf8ccda6a6",
+    "url": "/portfolio/static/media/SVG_ES6.fae70cd6.svg"
+  },
+  {
+    "revision": "0a3cf274c4be2e64da012984150fb73a",
+    "url": "/portfolio/static/media/SVG_Git.0a3cf274.svg"
+  },
+  {
+    "revision": "04726de8a2adb051d99df16d03afaed3",
+    "url": "/portfolio/static/media/SVG_HTML.04726de8.svg"
+  },
+  {
+    "revision": "00cebd558c9f5dc65e81984fd94a66a2",
+    "url": "/portfolio/static/media/SVG_JS.00cebd55.svg"
+  },
+  {
+    "revision": "9c5bd1934bebbaf0fe2154af204e94c7",
+    "url": "/portfolio/static/media/SVG_NPM.9c5bd193.svg"
+  },
+  {
+    "revision": "7d60d64b4c4f3aef9c46bb959da180af",
+    "url": "/portfolio/static/media/SVG_Photoshop.7d60d64b.svg"
+  },
+  {
+    "revision": "342149b6f7906bfbe6b9304031d263f1",
+    "url": "/portfolio/static/media/SVG_React.342149b6.svg"
+  },
+  {
+    "revision": "ff872163608b3ebdd35fc4e4e673754b",
+    "url": "/portfolio/static/media/SVG_Redux.ff872163.svg"
+  },
+  {
+    "revision": "04eb74feb072a072cfa9f0dcf96f973b",
+    "url": "/portfolio/static/media/SVG_Router.04eb74fe.svg"
+  },
+  {
+    "revision": "d0128a3b82e843574888215cb331f5f2",
+    "url": "/portfolio/static/media/SVG_Sass.d0128a3b.svg"
+  },
+  {
+    "revision": "0f72b09454ffdc54ba9714c4a7df7040",
+    "url": "/portfolio/static/media/SVG_Teamwork.0f72b094.svg"
+  },
+  {
+    "revision": "146e3e213de097b52817bc1b858780b4",
+    "url": "/portfolio/static/media/SVG_Vue.146e3e21.svg"
+  },
+  {
+    "revision": "a41096591f6b62f835d1b8307d7b5849",
+    "url": "/portfolio/static/media/SVG_Xd.a4109659.svg"
+  }
+]);
